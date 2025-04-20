54cd6eccfb9bdf6744171c8a7639b6fc3d132c9f
loppam <lolade132@gmail.com>
2025-04-20 15:25:56 +0100

    V3.8

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,24 +1,25 @@
 export async function GET() {
-  const appUrl = "https://h3lp3r.vercel.app/";
+  const appUrl = "https://h3lp3r.vercel.app";
 
   const config = {
     accountAssociation: {
       header:
-        "eyJmaWQiOjM2MjEsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyY2Q4NWEwOTMyNjFmNTkyNzA4MDRBNkVBNjk3Q2VBNENlQkVjYWZFIn0",
-      payload: "eyJkb21haW4iOiJmcmFtZXMtdjIudmVyY2VsLmFwcCJ9",
+        "eyJmaWQiOjMyNDAzNCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDM5OTgzOURlYUQ2NUIxNmVFOTdkOGRBZmI0NDVGMjQ0ODA5MUFhNTcifQ",
+      payload: "eyJkb21haW4iOiJoM2xwM3IudmVyY2VsLmFwcCJ9",
       signature:
-        "MHhiNDIwMzQ1MGZkNzgzYTExZjRiOTllZTFlYjA3NmMwOTdjM2JkOTY1NGM2ODZjYjkyZTAyMzk2Y2Q0YjU2MWY1MjY5NjI5ZGQ5NTliYjU0YzEwOGI4OGVmNjdjMTVlZTdjZDc2YTRiMGU5NzkzNzA3YzkxYzFkOWFjNTg0YmQzNjFi",
+        "MHhkZTU5NzY5ZjFlMDY0YmU4ZmY5MGVmYzdhNWI5OTA1MWE0OTBjNzUyODVkNzg5ZDUyZjViNjY4ZmU1N2UzYTIxM2Q1OTczMzcwZDI5YzRjYTI5NzJhYmExY2EwNDQzYmI5MTY1MGM0NmFkZDU0ZDgxNjEwMzYwM2I5NTk4ZmRkZDFi",
     },
     frame: {
-      version: "1",
+      version: "vNext",
       name: "H3LP3R",
-      iconUrl: `${appUrl}/images/logo.png`,
+      iconUrl: `${appUrl}/images/helper.png`,
       homeUrl: appUrl,
       imageUrl: `${appUrl}/images/helper.png`,
       buttonTitle: "H3LP",
       splashImageUrl: `${appUrl}/images/helper.png`,
-      splashBackgroundColor: "#f7f7f7",
-      webhookUrl: `${appUrl}/api/webhook`,
+      splashBackgroundColor: "#dfdfdf",
+      webhookUrl: `${appUrl}/api/frames/launch`,
+      authorizedDomains: ["h3lp3r.vercel.app"],
     },
   };
 
